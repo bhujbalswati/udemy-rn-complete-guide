263d81403c1c795b1427f070a263687737c67f64
Swati Bhujbal <swati.bhujbal@infogain.com>
2020-05-08 15:53:51 +0530

    This reverts commit a2d56880a2c1acf8138c82c4c75b96447c5d85ec. kept original files for reference

--- App.js
+++ App.js
@@ -1,30 +1,19 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import Header from './components/Header';
-import StartGameScreen from './screens/StartGameScreen';
-import GameScreen from './screens/GameScreen';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-
-  const startGameHandler = selectedNumber => {
-    setUserNumber(selectedNumber);
-  };
-
-  let content = <StartGameScreen onStartGame={startGameHandler}/>;
-  if(userNumber) {
-    content = <GameScreen userChoice={userNumber}/>;
-  }
   return (
-    <View style={styles.screen}>
-      <Header title="Guess the number"/>
-      {content}
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1
-  }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
